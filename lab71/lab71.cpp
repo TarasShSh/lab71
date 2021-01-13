fee67d774cb32f490acd1b464b0ed0c97cd4dfa3
Taras Shevchenko <taras.shevchenko.itis.2020@lpnu.ua>
2021-01-13 14:24:14 +0200

    frst

--- lab71/lab71.cpp
+++ lab71/lab71.cpp
@@ -1,61 +1,61 @@
 ﻿// lab71.cpp
 // Шевченко Тарас
-// Лабораторна робота № 7.1.
+// Лабораторна робота № 7.1
 // Пошук заданих елементів та впорядкування рядків / стовпчиків матриці.
-// Варіант 12
+// Варіант 11
 #include <iostream>
 #include <iomanip>
 #include <time.h>
 
 using namespace std;
 
-void Create(int** P, const int rowCount, const int colCount, int High, int Low) {
+void Create(int** Q, const int rowCount, const int colCount, int High, int Low) {
     for (int i = 0; i < rowCount; i++)
         for (int j = 0; j < colCount; j++)
-            P[i][j] = Low + rand() % (High - Low + 1);
+            Q[i][j] = Low + rand() % (High - Low + 1);
 }
 
-void Print(int** P, const int rowCount, const int colCount) {
+void Print(int** Q, const int rowCount, const int colCount) {
     cout << endl;
     for (int i = 0; i < rowCount; i++) {
         for (int j = 0; j < colCount; j++) {
-            cout << setw(4) << P[i][j];
+            cout << setw(4) << Q[i][j];
         }
         cout << endl;
     }
 }
 
-void Change(int** P, const int col1, const int col2, const int rowCount) {
+void Change(int** Q, const int col1, const int col2, const int rowCount) {
     int tmp;
     for (int i = 0; i < rowCount; i++) {
-        tmp = P[i][col1];
-        P[i][col1] = P[i][col2];
-        P[i][col2] = tmp;
+        tmp = Q[i][col1];
+        Q[i][col1] = Q[i][col2];
+        Q[i][col2] = tmp;
     }
 }
 
-void Sort(int** P, const int rowCount, const int colCount)
+void Sort(int** Q, const int rowCount, const int colCount)
 {
     for (int j0 = 0; j0 < colCount - 1; j0++)
         for (int j1 = 0; j1 < colCount - j0 - 1; j1++)
-            if ((P[0][j1] < P[0][j1 + 1])
+            if ((Q[0][j1] < Q[0][j1 + 1])
                 ||
-                (P[0][j1] == P[0][j1 + 1] &&
-                    P[1][j1] < P[1][j1 + 1])
+                (Q[0][j1] == Q[0][j1 + 1] &&
+                    Q[1][j1] > Q[1][j1 + 1])
                 ||
-                (P[0][j1] == P[0][j1 + 1] &&
-                    P[1][j1] == P[1][j1 + 1] &&
-                    P[2][j1] > P[2][j1 + 1]))
-                Change(P, j1, j1 + 1, rowCount);
+                (Q[0][j1] == Q[0][j1 + 1] &&
+                    Q[1][j1] == Q[1][j1 + 1] &&
+                    Q[2][j1] > Q[2][j1 + 1]))
+                Change(Q, j1, j1 + 1, rowCount);
 }
 
-void Sum(int** P, const int rowCount, const int colCount, int& S, int& n) {
+void Sum(int** Q, const int rowCount, const int colCount, int& S, int& n) {
     for (int i = 0; i < rowCount; i++)
         for (int j = 0; j < colCount; j++) {
-            if (P[i][j] % 3 == 0 && P[i][j] > 0) {
-                S += P[i][j];
+            if ( Q[i][j] % 2== 0 && Q[i][j] % 8 != 0) {
+                S += Q[i][j];
                 n++;
-                P[i][j] = 0;
+                Q[i][j] = 0;
             }
         }
 }
@@ -63,28 +63,28 @@ void Sum(int** P, const int rowCount, const int colCount, int& S, int& n) {
 
 int main()
 {
-    int Low = -21;
-    int High = 24;
+    int Low = 7;
+    int High = 62;
 
-    int rowCount = 9;
+    int rowCount = 7;
     int colCount = 6;
 
-    int** P = new int* [rowCount];
+    int** Q = new int* [rowCount];
     for (int i = 0; i < rowCount; i++)
-        P[i] = new int[colCount];
+        Q[i] = new int[colCount];
 
-    Create(P, rowCount, colCount, High, Low);
-    Print(P, rowCount, colCount);
-    Sort(P, rowCount, colCount);
-    Print(P, rowCount, colCount);
+    Create(Q, rowCount, colCount, High, Low);
+    Print(Q, rowCount, colCount);
+    Sort(Q, rowCount, colCount);
+    Print(Q, rowCount, colCount);
 
     int S = 0;
     int n = 0;
 
-    Sum(P, rowCount, colCount, S, n);
+    Sum(Q, rowCount, colCount, S, n);
     cout << "S = " << S << endl;
     cout << "n = " << n << endl;
-    Print(P, rowCount, colCount);
+    Print(Q, rowCount, colCount);
 
 
     system("pause");
